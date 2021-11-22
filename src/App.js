b552f61060cffb002b5f27e97bfbd495d061cdbb
apabaad <apabaad@yahoo.com>
2021-11-22 12:48:04 +1100

    react-router-dom and fontawesome done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.scss';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Hello world
+      <i class="far fa-sun"></i>
+    </div>
+  );
 }
 
 export default App;
